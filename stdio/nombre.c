9a688b40629a1fe6345e87e952cffd3737cea5de
Niz Nuñez <niizhe16@gmail.com>
2017-01-25 20:10:50 -0600

    HW about stdio

--- stdio/nombre.c
+++ stdio/nombre.c
@@ -2,10 +2,20 @@
 
 int main(int argc, char *argv[]) {
     char name[100]; 
+    int a;
+    int b;
+    int suma;
+
     fprintf(stdout, "Cual es tu nombre: ");
     fscanf (stdin, "%s", name);
     fprintf(stdout, "Hola %s \n ", name); 
+    fprintf(stdout, "Ingresa dos numeros: \n ");
+    fscanf (stdin, "%d %d", &a, &b);
 
+    suma = a + b;
 
+    fprintf(stdout, "%d, %d \n", a, b);
+    fprintf(stdout, "la suma de los numeros es:  %d + %d = %d. \n", a, b, suma);
+    
     return 0;
 }
--- stdio/nombre.py
+++ stdio/nombre.py
@@ -1,3 +1,11 @@
 name = raw_input("Cual es tu nombre: ")
 print "Hola " + name 
 
+a = int(input("ingresa numero1: "))
+
+b = int(input("ingresa numero2: "))
+
+suma = a + b
+
+print "Numero 1= " + str(a) + " Numero 2= " + str(b)
+print "La suma es = " + str(suma)
